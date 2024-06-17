5d41e3524b91f725a10c76e459f7ac14671b2737
Hugo Camelo dos Santos <ihugocs@gmail.com>
2024-06-17 14:07:14 -0400

    testando urls

--- projeto/urls.py
+++ projeto/urls.py
@@ -1,9 +1,21 @@
 from django.contrib import admin
 from django.urls import path
+from django.http import HttpResponse
 
+def home(request):
+    return HttpResponse("Home.")
 
+def sobre(request):
+    return HttpResponse("Sobre.")
+
+def contato(request):
+    return HttpResponse("Contato.")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home), # Home
+    path('sobre/', sobre), # Sobre
+    path('contato/', contato) # Contato 
 
 ]
+ 
